d4af91ded40837c81f767e2cb9af65479c82f9c3
ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>
2025-02-23 23:48:36 +0530

    added chrome-aws-lambda
    Signed-off-by: ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>

--- webhook/src/services/imageGen.ts
+++ webhook/src/services/imageGen.ts
@@ -1,19 +1,30 @@
 import puppeteer from "puppeteer";
+import chromium from "chrome-aws-lambda";
 import dotenv from "dotenv";
 dotenv.config();
 
 export default async function generateInvoiceImage(invoiceUrl: string) {
   
-  console.log("Chromium path:", await puppeteer.executablePath());
+  // console.log("Chromium path:", await puppeteer.executablePath());
+  const executablePath = await chromium.executablePath; // ✅ Get serverless-compatible Chromium path
+  console.log("Using Chromium Path:", executablePath);
+
+  
   const browser = await puppeteer.launch({
     headless: true,
-    args: [
-      "--no-sandbox", 
-      "--disable-setuid-sandbox"
-    ],
-    // executablePath: "/home/sbx_user1051/.cache/puppeteer/chrome/linux-133.0.6943.98/chrome-linux64/chrome",
-    executablePath: puppeteer.executablePath()
+    args: chromium.args,
+    executablePath
   });
+
+  // const browser = await puppeteer.launch({
+  //   headless: true,
+  //   args: [
+  //     "--no-sandbox", 
+  //     "--disable-setuid-sandbox"
+  //   ],
+  //   // executablePath: "/home/sbx_user1051/.cache/puppeteer/chrome/linux-133.0.6943.98/chrome-linux64/chrome",
+  //   executablePath: puppeteer.executablePath()
+  // });
   const page = await browser.newPage();
   await page.goto(invoiceUrl, { waitUntil: "networkidle0" });
 
