44b592035d43aa130c223f4ae03c0c059627b324
ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>
2025-02-23 23:30:04 +0530

    test
    Signed-off-by: ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>

--- webhook/src/services/imageGen.ts
+++ webhook/src/services/imageGen.ts
@@ -4,20 +4,20 @@ dotenv.config();
 
 export default async function generateInvoiceImage(invoiceUrl: string) {
   
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: [
-      "--no-sandbox", 
-      "--disable-setuid-sandbox"
-    ],
-    executablePath: "/tmp/puppeteer-cache/chrome/linux-133.0.6943.98/chrome-linux64/chrome",
-  });
+  // const browser = await puppeteer.launch({
+  //   headless: true,
+  //   args: [
+  //     "--no-sandbox", 
+  //     "--disable-setuid-sandbox"
+  //   ],
+  //   executablePath: "/tmp/puppeteer-cache/chrome/linux-133.0.6943.98/chrome-linux64/chrome",
+  // });
   console.log("Chromium path:", await puppeteer.executablePath());
-  const page = await browser.newPage();
-  await page.goto(invoiceUrl, { waitUntil: "networkidle0" });
+  // const page = await browser.newPage();
+  // await page.goto(invoiceUrl, { waitUntil: "networkidle0" });
 
-  const image = await page.screenshot({ encoding: "binary", fullPage: true });
+  // const image = await page.screenshot({ encoding: "binary", fullPage: true });
 
-  await browser.close();
-  return image;
+  // await browser.close();
+  return "";
 }
